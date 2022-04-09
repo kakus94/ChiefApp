3d8b19c186a6e95cb7c115fb1096ee79217b9dc2
Kamil <kkarpiak94@icloud.com>
2022-04-09 18:10:01 +0200

    Initial Commit

---
+++ ChiefApp/ChiefAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChiefAppApp.swift
+//  ChiefApp
+//
+//  Created by Kamil Karpiak on 09/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChiefAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChiefApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChiefApp
+//
+//  Created by Kamil Karpiak on 09/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
